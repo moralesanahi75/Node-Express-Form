2622a678d19c01a8b491084c6e2e40a0d39a0d7f
moralesanahi75 <moralesanahi75@hotmail.com>
2021-05-25 07:59:23 -0700

    recopied powerpint another side program
    let's do this!

--- index.js
+++ index.js
@@ -1,30 +1,44 @@
-< !DOCTYPE html >
-    <html>
-        <head>
-            <meta charset="utf-8">
-                <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                    <meta name="viewport" content="width=device-width, initial-scale=1">
-                        <meta name="author" content="Carlos Arias from Getbootstrap.com">
-                            <title>Express Form Example</title>
-                            <link href="http://leia.cs.spu.edu/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-</head>
-                            <body>
-                                <h1>Testing a Form with Express</h1>
-                                <div class="container">
-                                    <form action="process_get" method="GET">
-                                        <div class="form-group">
-                                            <label for="firstName">First Name:</label>
-                                            <input type="text" class="form-control" id="firstName" name="firstName">
-            </div>
-                                            <div class="form-group">
-                                                <label for="lastName">Last Name:</label>
-                                                <input type="text" class="form-control" id="lastName" name="lastName">
-            </div>
-                                                <div class="form-group">
-                                                    <input type="submit" value="Submit">
-            </div>
-        </form>
-                                            </div>
-                                            <h3>This is just random text</h3>
-</body>
-</html>
+var express = require('express');
+var app = express();
+
+app.all('/', function HandleAllRequests(request, response, next) {
+    console.log('Request from: ' + request.connection.remoteAddress);
+    next();
+});
+
+// This responds with "Hello World" on the homepage
+app.get('/', function HandleHomeGet(request, response) {
+    console.log("Got a GET request for the homepage");
+    response.send('Hello GET');
+})
+
+// This responds a POST request for the homepage
+app.post('/', function HandleHomePost(request, response) {
+    console.log("Got a POST request for the homepage");
+    response.send('Hello POST');
+})
+// This responds a DELETE request for the /del_user page.
+app.delete('/del_user', function HandleUserDel(request, response) {
+    console.log("Got a DELETE request for /del_user");
+    response.send('Hello DELETE');
+})
+
+// This responds a GET request for the /list_user page.
+app.get('/list_user', function HandleUserGet(request, response) {
+    console.log("Got a GET request for /list_user");
+    response.send('Page Listing');
+})
+
+// This responds a GET request for abcd, abxcd, ab123cd, and so on
+app.get('/ab*cd', function HandlePatterGet(request, response) {
+    console.log("Got a GET request for /ab*cd");
+    response.send('Page Pattern Match');
+})
+
+var server = app.listen(3000, function () {
+
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Example app listening at http://%s:%s", host, port)
+})
